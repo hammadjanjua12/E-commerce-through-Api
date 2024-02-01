9071772d282664aa9142151e8a3e616259c9d1b5
Hammad <hmmadakran786@gmail.com>
2024-02-01 22:38:14 +0500

    jewelry category will work

--- src/Navbar/Navigation.tsx
+++ src/Navbar/Navigation.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
+
+
 interface NavigationProps {
   cartCount: number;
 }
